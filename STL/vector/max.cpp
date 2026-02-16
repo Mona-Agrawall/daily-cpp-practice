dfcf3b47031364f6e588a175d1432733756c47b1
Mona <mona.mk.agrawal@gmail.com>
2026-02-17 00:53:07 +0530

    vector

--- STL/vector/max.cpp
+++ STL/vector/max.cpp
@@ -7,21 +7,27 @@ int main(){
     cout << "Enter the value of n: ";
     cin >> n;
 
+    if(n <= 0){
+        cout << "Invalid size";
+        return 0;
+    }
+
     vector<int> vec(n);
 
-    for(int i = 0; i<n ; i++){
-        cout << "Enter the elements: ";
+    // Take input
+    for(int i = 0; i < n; i++){
         cin >> vec[i];
-        cout << endl;
     }
-    int maxVal = vec[0];
 
-    for(int i = 0 ; i < n ; i++){
-        if(maxVal < vec[i]){
-            maxVal = vec[i];
+    int maximum = vec[0];
+
+    for(int i = 1; i < n; i++){
+        if(vec[i] > maximum){
+            maximum = vec[i];
         }
     }
 
-    cout << "The maximum element value is: " << maxVal ; 
+    cout << "Maximum element: " << maximum;
+
     return 0;
 }
