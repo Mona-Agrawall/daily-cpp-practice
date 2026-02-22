973001caaeb16fe4b7c4a0f6577466f4a952f146
Mona <mona.mk.agrawal@gmail.com>
2026-02-22 23:18:15 +0530

    Prime

--- STL/vector/vectorIterator.cpp
+++ STL/vector/vectorIterator.cpp
@@ -6,7 +6,16 @@ int main(){
     vector<int> v = {1,2,3,40};
 vector<int>::iterator it = v.begin();
 
-cout << *(it);
+for ( vector<int>::iterator it = v.begin() ; it < v.end() ; it++){
+    cout << *(it) << "    " << endl;
+}
+
+cout << "Using auto" << endl;
+
+for( auto it = v.begin();it < v.end() ; it++){
+    
+    cout << *(it) << " ";
+}
 
     return 0;
 }
