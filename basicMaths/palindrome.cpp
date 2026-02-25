07dc804aaca7a1102e9d0bc1fd84529205df67ec
Mona <mona.mk.agrawal@gmail.com>
2026-02-25 20:27:42 +0530

    Palindrome

--- basicMaths/palindrome.cpp
+++ basicMaths/palindrome.cpp
@@ -1,25 +1,53 @@
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int n;
+//     cout << "Enter the Number: ";
+//     cin >> n;
+//     int duplicate = n;
+
+//     int rev = 0;
+//     while(n>0){
+//         int last = n % 10;
+//         n = n/10;
+//         rev = (rev*10) + last;
+//     }
+
+//     cout << "The reverse number is: " << rev << endl;
+//     cout << "The original Number is: " << duplicate <<  endl;
+//     if(duplicate==rev){
+//         cout << "Yes the num is a palindrome.";
+//     }
+//     else{
+//         cout << "No the number is not a palindrome. ";
+//     }
+//     return 0;
+// }
+
+
 #include<iostream>
 using namespace std;
 int main(){
     int n;
-    cout << "Enter the Number: ";
+    cout << "Enter the number: "; //121
     cin >> n;
-    int duplicate = n;
 
-    int rev = 0;
+    int duplicate = n;
+    //for extraction of the numbers
+    int reverse = 0;
     while(n>0){
-        int last = n % 10;
+        int last = n%10;
+        
+        reverse = (reverse*10) + last;
         n = n/10;
-        rev = (rev*10) + last;
     }
 
-    cout << "The reverse number is: " << rev << endl;
-    cout << "The original Number is: " << duplicate <<  endl;
-    if(duplicate==rev){
-        cout << "Yes the num is a palindrome.";
+    if( duplicate == reverse){
+        cout << "The given number is palindrome. ";
     }
     else{
-        cout << "No the number is not a palindrome. ";
+        cout << "The given number is not a palindrome. ";
     }
+    
     return 0;
 }
